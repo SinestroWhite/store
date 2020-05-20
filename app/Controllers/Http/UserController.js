748f54c0d2c606be2085f1c6fa176b434a684040
SinestroWhite <g.yokov@hotmail.com>
2020-05-20 12:28:58 +0300

    I have added а Captcha

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,4 +1,6 @@
 'use strict'
+
+const Axios = use('axios');
 const Persona = use('Persona');
 const Env = use('Env');
 const Event = use('Event');
@@ -20,11 +22,34 @@ Persona.loginRules = function () {
 
 class UserController {
 
+    async verifyToken(token) {
+        return await Axios({
+            url: 'https://www.google.com/recaptcha/api/siteverify',
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            params: {
+                response: token,
+                secret: Env.get('GOOGLE_RECAPTCHA_V3_SECRET_KEY')
+            },
+        });
+    }
+
     async displayLogin({ view, auth, response })  {
-        return !auth.user ? view.render('login') : response.redirect('/');
+        return !auth.user ? view.render('login', {key: Env.get('GOOGLE_RECAPTCHA_V3_SITE_KEY'), useCaptcha: Env.get('USE_GOOGLE_RECAPTCHA')}) : response.redirect('/');
     }
 
     async login({ request, auth, response, session }) {
+        if (Env.get('USE_GOOGLE_RECAPTCHA') === 'true') {
+            await this.verifyToken(request.input('g-recaptcha-response')).then((response) => {
+                if (!response.data.success) {
+                    session.flash({loginError: 'Invalid ReCaptcha Response.'})
+                    return response.redirect('/login');
+                }
+            });
+        }
+
         try {
             let payload = request.only(['email', 'password'])
             payload.uid = payload.email;
@@ -39,10 +64,19 @@ class UserController {
     }
 
     async displayRegister({ view, auth, response })  {
-        return !auth.user ? view.render('register') : response.redirect('/');
+        return !auth.user ? view.render('register', {key: Env.get('GOOGLE_RECAPTCHA_V3_SITE_KEY'), useCaptcha: Env.get('USE_GOOGLE_RECAPTCHA')}) : response.redirect('/');
     }
 
     async register({ request, session, response, auth }) {
+        if (Env.get('USE_GOOGLE_RECAPTCHA') === 'true') {
+            await this.verifyToken(request.input('g-recaptcha-response')).then((response) => {
+                if (!response.data.success) {
+                    session.flash({loginError: 'Invalid ReCaptcha Response.'})
+                    return response.redirect('/register');
+                }
+            });
+        }
+
         const payload = request.only(['username', 'email', 'password', 'password_confirmation'])
         const user = await Persona.register(payload)
 
@@ -60,6 +94,15 @@ class UserController {
     }
 
     async forgotPassword({ request, session, response }) {
+        if (Env.get('USE_GOOGLE_RECAPTCHA') === 'true') {
+            await this.verifyToken(request.input('g-recaptcha-response')).then((response) => {
+                if (!response.data.success) {
+                    session.flash({loginError: 'Invalid ReCaptcha Response.'})
+                    return response.redirect('back');
+                }
+            });
+        }
+
         await Persona.forgotPassword(request.input('email'));
 
         session.flash({ message: 'We have sent you a recovery email!', type:'success' });
@@ -67,7 +110,7 @@ class UserController {
     }
 
     async displayForgotPassword({ view, auth, response })  {
-        return !auth.user ? view.render('forgot') : response.redirect('/');
+        return !auth.user ? view.render('forgot', {key: Env.get('GOOGLE_RECAPTCHA_V3_SITE_KEY'), useCaptcha: Env.get('USE_GOOGLE_RECAPTCHA')}) : response.redirect('/');
     }
 
     async displayResetPassword({ view, auth, response, params })  {
